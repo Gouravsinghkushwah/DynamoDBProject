465966cd673355aef48cbfd39e3617d79faae551
GouravSinghKushwah <gouravsingh1729@gmail.com>
2025-09-17 21:22:32 +0530

    change name Controller to UserController

--- src/main/java/com/dynamoDB/DynamoDBProject/controller/UserController.java
+++ src/main/java/com/dynamoDB/DynamoDBProject/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
-public class Controller {
+public class UserController {
 
     private UserService service;
 
